00d7ac59e7bcb213d1fba0d4e44ec1bbc8c531bd
JackLindgren <jtlindgren88@gmail.com>
2016-04-20 21:00:15 -0700

    44 wip

--- prob44.py
+++ prob44.py
@@ -1,12 +1,25 @@
+import math
+
 pentNums = []
 
 for n in range(1,1000000):
 	pnum = (n * (3 * n - 1)) / 2
 	pentNums.append(pnum)
 
-i = 0
-while (pentNums[i] + pentNums[i + 1] not in pentNums) and (pentNums[i+1] - pentNums[i] not in pentNums):
-	i += 1
+def isPentNum(num):
+  pentest = (math.sqrt(1 + 24 * num) + 1 ) / 6
+  return pentest == int(pentest)
 
-print pentNums[i], pentNums[i + 1]
-print pentNums[i + 1] - pentNums[i]
+for o in pentNums:
+  for i in pentNums:
+    if isPentNum(o + i):
+      if isPentNum(abs(o - i)):
+        print o, i
+
+
+#i = 0
+#while (pentNums[i] + pentNums[i + 1] not in pentNums) and (pentNums[i+1] - pentNums[i] not in pentNums):
+#	i += 1
+
+#print pentNums[i], pentNums[i + 1]
+#print pentNums[i + 1] - pentNums[i]
