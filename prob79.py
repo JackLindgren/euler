0d31b2288200d966d55316cc919465283fd06206
allabouttoledo <jtlindgren88@gmail.com>
2016-04-24 21:32:59 -0700

    79 done

---
+++ prob79.py
@@ -0,0 +1,36 @@
+tries = ["319", "680", "180", "690", "129", "620", "762", "689", "762", "318", "368", "710", "720", "710", "629", "168", "160", "689", "716", "731", "736", "729", "316", "729", "729", "710", "769", "290", "719", "680", "318", "389", "162", "289", "162", "718", "729", "319", "790", "680", "890", "362", "319", "760", "316", "729", "380", "319", "728", "716"]
+
+pairs = []
+
+def dedupe(lol): # list of lists
+	result = []
+	for l in lol:
+		if l not in result:
+			result.append(l)
+	return result
+
+for o in range(0,10):
+	for i in range(0,10):
+		for t in tries:
+			if str(o) in t and str(i) in t:
+				if list(t).index(str(o)) > list(t).index(str(i)):
+					pairs.append([i, o])
+				elif list(t).index(str(i)) > list(t).index(str(o)):
+					pairs.append([o, i])
+
+for p in dedupe(pairs):
+	print p
+
+for i in range(0,10):
+	numAfter = []
+	for p in dedupe(pairs):
+		if p[0] == i:
+			numAfter.append(p[1])
+	print "Comes after  {0}:".format(i), numAfter
+
+for i in range(0,10):
+	numBefore = []
+	for p in dedupe(pairs):
+		if p[1] == i:
+			numBefore.append(p[0])
+	print "Comes before {0}:".format(i), numBefore
